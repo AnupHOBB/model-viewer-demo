1aaada7612a9df1d14a23c488cf4b12c4a4b313e
Anup Bhandari <anup.b@HOBB.COM>
2025-05-14 13:19:30 +0530

    Reduced line thickness to 1/10 of size

--- Dimension.js
+++ Dimension.js
@@ -47,7 +47,7 @@ class Dimension
         this.lineMaterial = new THREE.LineBasicMaterial({
             color: new THREE.Color(1, 0, 0), 
             vertexColors: true, 
-            linewidth: this._isHandHeldDevice() ? 1 : 5
+            linewidth: this._isHandHeldDevice() ? 0.01 : 5
         })
         this.lineMaterial.linewidth = 10
         this.lineMaterial.worldUnits = true
