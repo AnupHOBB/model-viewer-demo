927957b4204ece3cd0b8f198d352f8ed3c5b4aa1
Anup Bhandari <anup.b@HOBB.COM>
2025-05-14 13:24:39 +0530

    Reduced line thickness to 1 on all devices

--- Dimension.js
+++ Dimension.js
@@ -47,9 +47,9 @@ class Dimension
         this.lineMaterial = new THREE.LineBasicMaterial({
             color: new THREE.Color(1, 0, 0), 
             vertexColors: true, 
-            linewidth: 5
+            linewidth: 1
         })
-        this.lineMaterial.linewidth = this._isHandHeldDevice() ? 1 : 10
+        this.lineMaterial.linewidth = 1
         this.lineMaterial.worldUnits = true
         this.lineMaterial.needsUpdate = true
     
